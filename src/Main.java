65f72e0668005a0904b8f518c7f1561a4d1e47fc
MykolaMurza <murza.na00@gmail.com>
2023-02-25 21:21:49 +0200

    lesson 11 prepare

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import java.util.function.BiFunction;
-
 public class Main {
     public static void main(String[] args) {
-        BiFunction<Integer, Integer, Double> func = (x, y) -> (double) x / y;
-        System.out.println(
-                func.apply(10, 2)
-        );
+
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,44 @@
+public class Student {
+    private String name;
+    private String surname;
+    private int yearOfStudy;
+
+    public Student(String name, String surname, int yearOfStudy) {
+        this.name = name;
+        this.surname = surname;
+        this.yearOfStudy = yearOfStudy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYearOfStudy() {
+        return yearOfStudy;
+    }
+
+    public void setYearOfStudy(int yearOfStudy) {
+        this.yearOfStudy = yearOfStudy;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", yearOfStudy=" + yearOfStudy +
+                '}';
+    }
+}
